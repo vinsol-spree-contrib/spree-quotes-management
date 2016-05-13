7f4d8cdae9cc9b1a0d696a40dbdd821eef889523
Pratibha <pratibha@Pratibhas-MacBook-Pro.local>
2016-05-13 13:27:52 +0530

    Fix Quotes CRUD at user end

--- app/assets/javascripts/spree/frontend/spree_quotes_management.js
+++ app/assets/javascripts/spree/frontend/spree_quotes_management.js
@@ -1,2 +1,35 @@
 // Placeholder manifest file.
 // the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/frontend/all.js'
+
+
+function SpreeQuotesManagement(max_limit, span_char_left, quotes_textarea) {
+  this.max_limit = max_limit;
+  this.$span_char_left = $(span_char_left);
+  this.$quotes_textarea = $(quotes_textarea)
+}
+
+SpreeQuotesManagement.prototype.init = function() {
+  var _this = this;
+  _this.$span_char_left.css('display', 'none');
+
+  _this.$quotes_textarea.keyup(function () {
+    var lengthCount = this.value.length;
+    if (lengthCount > _this.max_limit) {
+      this.value = this.value.substring(0, _this.max_limit);
+      var charactersLeft = _this.max_limit - lengthCount + 1;
+    }
+    else {
+      var charactersLeft = _this.max_limit - lengthCount;
+    }
+    _this.$span_char_left.css('display', 'block');
+    _this.$span_char_left.text(charactersLeft + ' Characters left');
+  });
+}
+
+
+$(function () {
+  var max_limit = 240;
+  var span_char_left = $('#spnCharLeft');
+  var quotes_textarea = $('#quote_description');
+  new SpreeQuotesManagement(max_limit, span_char_left, quotes_textarea).init()
+});
--- app/models/spree/quote.rb
+++ app/models/spree/quote.rb
@@ -5,18 +5,32 @@ module Spree
 
     belongs_to :user
 
+    before_destroy :check_if_published
+
     state_machine initial: :draft do
+
+      before_transition on: :publish, do: :set_date_of_publish
 
       state :draft do
         transition to: :publish, on: :publish
       end
 
       state :publish do
-        validates_presence_of :rank
         transition to: :draft, on: :unpublish
       end
-
     end
 
+    private
+
+      def check_if_published
+        if publish?
+          errors.add(:Base, Spree.t(:destroy_published_quote))
+          false
+        end
+      end
+
+      def set_date_of_publish
+        self.published_at = Time.now
+      end
   end
 end
