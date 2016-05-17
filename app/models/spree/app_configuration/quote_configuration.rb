5b400501cb82898caaea84ad2fa6183fe7c2f770
Pratibha <pratibha@Pratibhas-MacBook-Pro.local>
2016-05-17 16:02:14 +0530

    Add disable functionality to quotes

--- app/models/spree/app_configuration/quote_configuration.rb
+++ app/models/spree/app_configuration/quote_configuration.rb
@@ -3,5 +3,6 @@ module SpreeQuotesManagement
     preference :max_char_in_quotes, :integer, default: 240
     preference :admin_quotes_per_page, :integer, default: 10
     preference :quotes_count, :integer, default: 5
+    preference :disable_quote?, :boolean, default: false
   end
 end
