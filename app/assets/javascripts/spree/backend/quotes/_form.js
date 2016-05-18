9e3a81fc5308e1348764c5d0da1824e6254042e6
Pratibha <pratibha@Pratibhas-MacBook-Pro.local>
2016-05-18 18:11:18 +0530

    precompile assets

---
+++ app/assets/javascripts/spree/backend/quotes/_form.js
@@ -0,0 +1,5 @@
+//= require spree/characters_left_counter
+
+$(function() {
+  new CharactersLeftCounter('#quote_description_field').init();
+});
--- app/assets/javascripts/spree/frontend/spree_quotes_management.js
+++ app/assets/javascripts/spree/frontend/spree_quotes_management.js
@@ -1,8 +1,2 @@
 // Placeholder manifest file.
 // the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/frontend/all.js'
-
-//= require spree/characters_left_counter
-
-$(function() {
-  new CharactersLeftCounter('#user_quotes_new_form').init();
-});
