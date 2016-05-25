859141cbb2087506f8c1605283d51105b26fab0c
Pratibha <pratibha@Pratibhas-MacBook-Pro.local>
2016-05-25 11:42:16 +0530

    Fix specs

--- spec/presenters/quote_presenter_spec.rb
+++ spec/presenters/quote_presenter_spec.rb
@@ -7,13 +7,13 @@ describe Spree::QuotePresenter do
 
   describe "#author_name" do
     context 'when author_name is blank' do
-      it "returns decorated author_name" do
+      it "returns  Anonymous" do
         expect(present(quote).author_name).to eq('Anonymous')
       end
     end
 
     context 'when author_name is present' do
-      it "returns decorated author_name" do
+      it "returns author_name" do
         expect(present(quote1).author_name).to eq('abc')
       end
     end
@@ -21,13 +21,13 @@ describe Spree::QuotePresenter do
 
   describe "#published_at" do
     context 'when published_at is blank' do
-      it "returns decorated published_at" do
+      it "returns '-' " do
         expect(present(quote).published_at).to eq('-')
       end
     end
 
     context 'when published_at is present' do
-      it "returns decorated published_at" do
+      it "returns published_at" do
         quote1.publish
         expect(present(quote1).published_at).to eq(pretty_time(quote1.published_at))
       end
@@ -36,7 +36,7 @@ describe Spree::QuotePresenter do
 
   describe "#rank" do
     context 'when rank is blank' do
-      it "returns decorated rank" do
+      it "returns '-'" do
         expect(present(quote).rank).to eq('-')
       end
     end
@@ -47,7 +47,7 @@ describe Spree::QuotePresenter do
         quote1.update(rank: 1)
       end
 
-      it "returns decorated rank" do
+      it "returns rank" do
         expect(present(quote1).rank).to eq(1)
       end
     end
